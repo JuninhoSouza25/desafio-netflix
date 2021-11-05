cd0cf8a9adf44769c10bba41fe79901a4964fb1d
Juninho Souza <dnt-bh@hotmail.com>
2021-11-05 09:09:29 -0300

    Evento de click e mudanca de cor nas estrelas

---
+++ js/owl/star.js
@@ -0,0 +1,48 @@
+const s1 = document.querySelector("#s-1")
+const s2 = document.querySelector("#s-2")
+const s3 = document.querySelector("#s-3")
+const s4 = document.querySelector("#s-4")
+const s5 = document.querySelector("#s-5")
+
+const star = document.querySelector("#star-container")
+
+
+s1.addEventListener("click", function(){
+
+    s1.style.color = "#e50914"
+
+});
+
+s2.addEventListener("click", function(){
+
+    s1.style.color = "#e50914"
+    s2.style.color = "#e50914"
+
+});
+
+s3.addEventListener("click", function(){
+
+    s1.style.color = "#e50914"
+    s2.style.color = "#e50914"
+    s3.style.color = "#e50914"
+
+});
+
+s4.addEventListener("click", function(){
+
+    s1.style.color = "#e50914"
+    s2.style.color = "#e50914"
+    s3.style.color = "#e50914"
+    s4.style.color = "#e50914"
+
+});
+
+s5.addEventListener("click", function(){
+
+    s1.style.color = "#e50914"
+    s2.style.color = "#e50914"
+    s3.style.color = "#e50914"
+    s4.style.color = "#e50914"
+    s5.style.color = "#e50914"
+
+});
